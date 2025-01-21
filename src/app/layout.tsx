3ff4e8d06755f276b535c29ae62a71042023e116
gotospacemedia <gotospace.co@gmail.com>
2025-01-21 18:16:25 +0600

    modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 
 import "./globals.css";
+import Footer from "../components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
